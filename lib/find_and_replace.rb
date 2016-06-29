69b0e27a665be08f178bee774ef93ffdf0d2a281
Matt Carlson and Molly McGlone <matthewthomascarlson@gmail.com>
2016-06-29 14:09:57 -0700

    get find_and_replace method functioning

--- lib/find_and_replace.rb
+++ lib/find_and_replace.rb
@@ -2,17 +2,48 @@ require('pry')
 
 class String
   define_method(:find_and_replace) do |word1, word2|
-    word_array = self.split(' ')
+    word_array = self.downcase().split(' ')
     new_word_array = []
+    word1_lower = word1.downcase()
+    word2_lower = word2.downcase()
     word_array.each do |word|
-      if word == word1
-        # word = word2
-        new_word_array.push(word2)
+      if word.include?(word1_lower)
+        position = word.index(word1_lower)
+        value_to_add = word1_lower.length - 1
+        end_position = position + value_to_add
+        word[position..end_position] = word2_lower
+        new_word_array.push(word)
       else
         new_word_array.push(word)
       end
-      # new_word_array.concat(word)
     end
-    new_word_array.join(' ')
+    new_word_array.join(' ').capitalize()
   end
 end
+
+
+
+
+
+# #  example_word = self
+#   if example_word.include?(word1_lower)
+#     position = example_word.index(word1)
+#     value_to_add = word1.length - 1
+#     end_position = position + value_to_add
+#     example_word[position..end_position] = word2_l
+#     example_word
+#   else
+#     example_word
+#   end
+#
+#
+# word_array.each do |word|
+#   if word == word1
+#     # word = word2
+#     new_word_array.push(word2)
+#   else
+#     new_word_array.push(word)
+#   end
+#   # new_word_array.concat(word)
+# end
+# new_word_array.join(' ')
--- spec/find_and_replace_spec.rb
+++ spec/find_and_replace_spec.rb
@@ -3,9 +3,15 @@ require('find_and_replace')
 
 describe('String#find_and_replace') do
   it('replaces words in a string with a specified word') do
-    expect(('the world is great').find_and_replace('great', 'bad')).to(eq("the world is bad"))
+    expect(('the world is great').find_and_replace('great', 'bad')).to(eq("The world is bad"))
   end
   it('handles partial matches') do
-    expect(("I am walking my cat to the cathedral").find_and_replace("cat", "dog")).to(eq("I am walking my dog to the doghedral."))
+    expect(("I am walking my cat to the cathedral.").find_and_replace("cat", "dog")).to(eq("I am walking my dog to the doghedral."))
+  end
+  it('handles all caps entries') do
+    expect(("I AM ON FIRE").find_and_replace("am", "am not")).to(eq("I am not on fire"))
+  end
+  it('handles mixed case entries') do
+    expect(("wHaT iS yOuR nAmE?").find_and_replace('yOuR', 'my')).to(eq("What is my name?"))
   end
 end
