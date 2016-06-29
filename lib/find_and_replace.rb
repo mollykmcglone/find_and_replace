e71e134ae7f1bcafb3c15560fa922633b2a16cb2
Matt Carlson and Molly McGlone <mollykmcglone@gmail.com>
2016-06-29 11:54:56 -0700

    get first test passing

--- lib/find_and_replace.rb
+++ lib/find_and_replace.rb
@@ -1,1 +1,18 @@
 require('pry')
+
+class String
+  define_method(:find_and_replace) do |word1, word2|
+    word_array = self.split(' ')
+    new_word_array = []
+    word_array.each do |word|
+      if word == word1
+        # word = word2
+        new_word_array.push(word2)
+      else
+        new_word_array.push(word)
+      end
+      # new_word_array.concat(word)
+    end
+    new_word_array.join(' ')
+  end
+end
--- spec/find_and_replace_spec.rb
+++ spec/find_and_replace_spec.rb
@@ -1,2 +1,11 @@
 require('rspec')
 require('find_and_replace')
+
+describe('String#find_and_replace') do
+  it('replaces words in a string with a specified word') do
+    expect(('the world is great').find_and_replace('great', 'bad')).to(eq("the world is bad"))
+  end
+  it('handles partial matches') do
+    expect(("I am walking my cat to the cathedral").find_and_replace("cat", "dog")).to(eq("I am walking my dog to the doghedral."))
+  end
+end
